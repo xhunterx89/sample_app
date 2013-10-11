8f859a385ec38a853c5028a6791876e40690ef10
pxthanh <pxthanh123@gmail.com>
2013-10-11 10:46:27 +0700

    all

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,18 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page]).per_page(10)
+    end
+  end
+  
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
